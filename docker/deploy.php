e23052b1067daa43449d5773a229d1eb9d0be704
William Hearn <sylus1984@gmail.com>
2017-04-24 23:30:31 -0400

    feat(deployer): Minor deployer fixes for env

--- docker/deploy.php
+++ docker/deploy.php
@@ -5,8 +5,10 @@ namespace Deployer;
 require 'recipe/drupal8.php';
 require 'vendor/deployer/recipes/slack.php';
 
-set('repository', 'http://github.com/open-data/site-open-data.git');
-set('env_vars', "HTTP_PROXY=".getenv('HTTP_PROXY'));
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+set('repository', 'https://github.com/open-data/site-open-data.git');
+set('env_vars', "HTTPS_PROXY=" . getenv('HTTPS_PROXY'));
 set('keep_releases', '15');
 
 # Override native deployer composer task for proxy support
@@ -16,8 +18,8 @@ set('bin/composer', function () {
         $composer = run('which composer')->toString();
     }
     if (empty($composer)) {
-        run("source ~/.profile && cd {{release_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}");
-        $composer = "source ~/.profile && {{bin/php}} {{release_path}}/composer.phar";
+        run("source ~/.profile && cd {{release_path}} && curl -sS https://getcomposer.org/installer | /opt/rh/rh-php56/root/usr/bin/php");
+        $composer = "source ~/.profile && /opt/rh/rh-php56/root/usr/bin/php {{release_path}}/composer.phar";
     }
     return $composer;
 });
@@ -61,5 +63,56 @@ set('slack', [
       'url' => getenv('PROXY_URL'),
     ],
 ]);
+
+task('deploy:update_code', function () {
+    $repository = trim(get('repository'));
+    $branch = get('branch');
+    $git = get('bin/git');
+    $gitCache = get('git_cache');
+    $depth = $gitCache ? '' : '--depth 1';
+    $options = [
+        'tty' => get('git_tty', false),
+    ];
+    // If option `branch` is set.
+    if (input()->hasOption('branch')) {
+        $inputBranch = input()->getOption('branch');
+        if (!empty($inputBranch)) {
+            $branch = $inputBranch;
+        }
+    }
+    // Branch may come from option or from configuration.
+    $at = '';
+    if (!empty($branch)) {
+        $at = "-b $branch";
+    }
+    // If option `tag` is set
+    if (input()->hasOption('tag')) {
+        $tag = input()->getOption('tag');
+        if (!empty($tag)) {
+            $at = "-b $tag";
+        }
+    }
+    // If option `tag` is not set and option `revision` is set
+    if (empty($tag) && input()->hasOption('revision')) {
+        $revision = input()->getOption('revision');
+        if (!empty($revision)) {
+            $depth = '';
+        }
+    }
+    if ($gitCache && has('previous_release')) {
+        try {
+            run("source ~/.profile && $git clone $at --recursive -q --reference {{previous_release}} --dissociate $repository  {{release_path}} 2>&1", $options);
+        } catch (\RuntimeException $exc) {
+            // If {{deploy_path}}/releases/{$releases[1]} has a failed git clone, is empty, shallow etc, git would throw error and give up. So we're forcing it to act without reference in this situation
+            run("source ~/.profile && $git clone $at --recursive -q $repository {{release_path}} 2>&1", $options);
+        }
+    } else {
+        // if we're using git cache this would be identical to above code in catch - full clone. If not, it would create shallow clone.
+        run("source ~/.profile && $git clone $at $depth --recursive -q $repository {{release_path}} 2>&1", $options);
+    }
+    if (!empty($revision)) {
+        run("source ~/.profile && cd {{release_path}} && $git checkout $revision");
+    }
+});
 
 after('deploy','deploy:slack');
